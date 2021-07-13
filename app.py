3b4f7f2f8e5a72f7fecbae2bbe51eec86d67333e
repo_bot <elvanselvano71@gmail.com>
2021-07-13 21:27:32 +0700

    update web layout

--- app.py
+++ app.py
@@ -12,26 +12,28 @@ def predict(model, df):
     predictions_data = predict_model(estimator=model, data=df)
     return predictions_data["Label"][0]
 
-
 # app title and description
 st.title("Washington D.C. Residential Properties Price Prediction 🏠")
-
+st.write(
+    """
+![GitHub](https://img.shields.io/github/watchers/elvanselvano/purwadhika-final-project?style=social)
+[![Status](https://img.shields.io/badge/status-active-success.svg)]() 
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE.md)
+"""
+)
 image = Image.open("./assets/wallpaper.jpg")
 st.image(image)
 
 st.markdown(
     """
-This is a web app to predict the price of a residential property based on\
-        the features that you can see in the sidebar. Please adjust the\
-        value of each feature. After that, click on the **Predict** button to\
-        see the prediction of the machine learning model.
-
-All data is available at [Open Data D.C.](https://opendata.dc.gov/). The residential and address point data is managed by the [Office of the Chief Technology Officer](https://octo.dc.gov/).
+This web app uses actual transaction data and machine learning models to predict the prices of housing in Washington, D.C., which is the capital of the United States. The actual transaction data contain attributes and transaction prices of real estate that respectively serve as independent variables and dependent variables for machine learning models.
 """
 )
+# st.write("---")
 
 # interior details
-st.sidebar.header("Interior details")
+st.sidebar.subheader("Interior Details")
+st.sidebar.write("")
 BEDRM = st.sidebar.slider(label="Bedroom", min_value=1, max_value=6, value=3, step=1)
 BATHRM = st.sidebar.slider(label="Bathroom", min_value=0, max_value=6, value=2, step=1)
 HF_BATHRM = st.sidebar.slider(
@@ -45,7 +47,8 @@ HEAT = st.sidebar.radio(
 )
 
 # additional features
-st.sidebar.header("Additional Features")
+st.sidebar.write("---")
+st.sidebar.subheader("Additional Features")
 st.sidebar.write("")
 ac_check = st.sidebar.checkbox("AC", value=False)
 AC = 1 if ac_check else 0
@@ -57,7 +60,9 @@ remodeled_check = st.sidebar.checkbox("Remodeled", value=False)
 RMDL = 1 if remodeled_check else 0
 
 # construction details
-st.sidebar.header("Construction details")
+st.sidebar.write("---")
+st.sidebar.subheader("Construction Details")
+st.sidebar.write("")
 STYLE = st.sidebar.selectbox(
     "Style", ("1 Story", "1.5 Story Fin", "2 Story", "2.5 Story Fin", "3 Story")
 )
@@ -81,8 +86,9 @@ ROOF = st.sidebar.selectbox(
 STRUCT = st.sidebar.selectbox(
     "Structure", ("Row Inside", "Single", "Semi-Detached", "Row End", "Multi")
 )
-
-st.sidebar.header("Property details")
+st.sidebar.write("---")
+st.sidebar.subheader("Property Details")
+st.sidebar.write("")
 AYB = st.sidebar.slider(label="AYB", min_value=1914, max_value=2018, value=1935, step=1)
 EYB = st.sidebar.slider(label="EYB", min_value=1964, max_value=2018, value=1972, step=1)
 GBA = st.sidebar.slider(label="GBA", min_value=1204, max_value=1800, value=1577, step=2)
@@ -108,6 +114,14 @@ GRADE = st.sidebar.selectbox(
 WARD = st.sidebar.selectbox(
     "Ward",
     ("Ward 1", "Ward 2", "Ward 3", "Ward 4", "Ward 5", "Ward 6", "Ward 7", "Ward 8"),
+)
+
+st.subheader("Property Information")
+st.markdown(
+    "👈  In the sidebar, there are 21 features that you can adjust. The features are shown\
+    in the table below. After you have adjusted the \
+    features, click on the **Predict** button to see the prediction of the machine\
+        learning model."
 )
 
 # mapping features with user's input
@@ -138,8 +152,18 @@ features = {
 # Converting Features into DataFrame
 
 features_df = pd.DataFrame([features])
-st.write("---")
+st.dataframe(features_df)
 
+# predict button
+if st.button("Predict"):
+    prediction = predict(model, features_df)
+    st.success(
+        "Based on the features, the price of the property is $"
+        + str(int(prediction))
+        + "."
+    )
+
+st.write("---")
 # Data Section
 expander_bar = st.beta_expander("Data Description")
 expander_bar.markdown(
@@ -170,13 +194,7 @@ expander_bar.markdown(
 
 """
 )
-st.dataframe(features_df)
-
-# predict button
-if st.button("Predict"):
-    prediction = predict(model, features_df)
-    st.success(
-        "Based on the features, the price of the property is $"
-        + str(int(prediction))
-        + "."
-    )
+expander_bar.write("")
+expander_bar.info(
+    "All data is available at [Open Data D.C.](https://opendata.dc.gov/). The residential and address point data is managed by the [Office of the Chief Technology Officer](https://octo.dc.gov/)."
+)
